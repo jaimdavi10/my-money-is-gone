54fcd0d7465771a71270afb03d7c2c760b1e6fc6
jaimdavi10 <78942351+jaimdavi10@users.noreply.github.com>
2021-03-24 19:05:29 -0500

    HW08P1Updates

--- src/main/java/com/mymoneyisgone/controllers/PurchaseEntryController.java
+++ src/main/java/com/mymoneyisgone/controllers/PurchaseEntryController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -33,6 +30,13 @@ import javax.validation.Valid;
             model.addAttribute("entry", new PurchaseEntry());
             return "purchase-entry";
 
+        }
+
+        @GetMapping("/view/{id}")
+        public String showPurchase (@PathVariable Long id, Model model){
+            PurchaseEntry purchaseEntry = this.per.findById(id).get();
+            model.addAttribute("purchaseEntry", purchaseEntry);
+            return "view-purchase";
         }
 
         @PostMapping
