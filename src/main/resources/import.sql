493159858a39331f6b38ccbf7d81d3a1c3a3d3eb
jaimdavi10 <78942351+jaimdavi10@users.noreply.github.com>
2021-04-08 15:12:59 -0500

    HW09P2Update

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO product_type (id , name) VALUES (1 , 'Food');
+INSERT INTO product_type (id , name) VALUES (2 , 'Entertainment');
+INSERT INTO product_type (id , name) VALUES (3 , 'Fashion');
+INSERT INTO product_type (id , name) VALUES (4 , 'Technology');
+INSERT INTO product_type (id , name) VALUES (5 , 'Outings');
+INSERT INTO product_type (id , name) VALUES (6 , 'Other');
